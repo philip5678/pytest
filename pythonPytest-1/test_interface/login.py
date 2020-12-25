313df68c42cb0c7d886ea37c456b41c653c0168b
Philip Zheng <optionabc@gmail.com>
2020-12-25 13:50:43 -0500

    1

--- pythonPytest-1/test_interface/login.py
+++ pythonPytest-1/test_interface/login.py
@@ -4,7 +4,7 @@ import requests
 from requests import request
 
 
-def login(s):
+def login(s, userName='bob', password='123'):
     '''
     :param
     '''
@@ -14,8 +14,8 @@ def login(s):
 
     userInfo = {
         "_id": 'string',
-        "userName": 'bob',
-        "password": '123',
+        "userName": userName,
+        "password": password,
         "password2": 'string',
         "fullName": 'string',
         "role": 'string',
@@ -28,6 +28,7 @@ def login(s):
         'tag': '',
     }
 
+    # ________________________________________________________________________________
     # res_Login = request('post', loginUrl,
     #                     json=userInfo)  # :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
     #
@@ -36,13 +37,14 @@ def login(s):
     # token = res_Login.json()['token']
     # print(f"{token}")
     #
-    print(request('get', postsUrl, params=queryParams))
+    # print(request('get', postsUrl, params=queryParams))
     # res_GetPosts = request('get', postsUrl, params=queryParams).text
     # print(res_GetPosts)
     #
     # print(request('get', postsUrl, params=queryParams).json())
     # print(json.loads(request('get', postsUrl, params=queryParams).text))
 
+    # ________________________________________________________________________________
     r = s.post(loginUrl, json=userInfo)
     token = r.json()['token']
     print(r.json())
@@ -50,7 +52,44 @@ def login(s):
     r = s.get(postsUrl, params=queryParams)
     print(r.json())
 
+    # ________________________________________________________________________________
+    # from postman
+    # url = "https://web422blog.herokuapp.com/login"
+    # payload="{\r\n    \"userName\": \"rob\",\r\n    \"password\": \"123\"\r\n\r\n}"
+    # headers = {
+    #   'Content-Type': 'application/json'
+    # }
+    # response = requests.request("POST", url, headers=headers, data=payload)
+    # print(response.text)
+
+    # ________________________________________________________________________________
+    # from postman
+    # import http.client
+    #
+    # conn = http.client.HTTPSConnection("web422blog.herokuapp.com")
+    # payload = "{\r\n    \"userName\": \"rob\",\r\n    \"password\": \"123\"\r\n\r\n}"
+    # headers = {
+    #   'Content-Type': 'application/json'
+    # }
+    # conn.request("POST", "/login", payload, headers)
+    # res = conn.getresponse()
+    # data = res.read()
+    # print(data.decode("utf-8"))
+
+    # ________________________________________________________________________________
+    # url = "https://web422blog.herokuapp.com/admin"
+    # payload = "{\r\n    \"userName\": \"rob\",\r\n    \"password\": \"123\"\r\n\r\n}"
+    # headers = {
+    #     'Content-Type': 'application/json',
+    #     'Authorization': f'jwt {token}'
+    # }
+    # response = requests.request("GET", url, headers=headers, data=payload)
+    # print(response.text)
+
+    return token
+
 
 if __name__ == '__main__':
     session = requests.session()
-    login(session)
+    token = login(session)
+    print(token)
